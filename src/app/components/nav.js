23de3d3153ddcb2b0691c8a8eefbe274f45b96fc
joscript <yosefeid058@gmail.com>
2025-03-05 23:02:43 +0200

    jo

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -5,21 +5,20 @@ function nav() {
       class: "w-full h-20 bg-[#35736E] z-[9999] fixed top-0 flex items-center justify-between text-white px-[3%]", children: [
         jo('img', {src:"/img/FIN.png"}),
 
-       jo("div", {class:'flex items-center justify-center gap-[111px]', children: [
+       jo("div", {class:'flex items-center justify-center gap-[111px] max-[800px]:gap-[7%]', children: [
          // links
-         jo('div', {
-          class: 'flex gap-[34px] text-lg', children: [
-            jo("a", { t: "Home", href: "/", class:'hover:text-[#FECA11]' }),
-            jo("a", { t: "About Us", href: "/about", class:'hover:text-[#FECA11]' }),
-            jo("a", { t: "Services", href: "/services", class:'hover:text-[#FECA11]' }),
-            jo("a", { t: "Blog", href: "/blog", class:'hover:text-[#FECA11]' }),
-            jo("a", { t: "Contact Us", href: "/contact", class:'hover:text-[#FECA11]' }),
+         jo('div', {class: 'flex gap-[34px] text-lg max-[800px]:gap-[20px]  ', children: [
+            jo("a", { t: "Home", href: "/", class:'hover:text-[#FECA11] max-[800px]:text-sm' }),
+            jo("a", { t: "About", href: "/about", class:'hover:text-[#FECA11] max-[800px]:text-sm' }),
+            jo("a", { t: "Services", href: "/services", class:'hover:text-[#FECA11] max-[800px]:text-sm' }),
+            jo("a", { t: "Blog", href: "/blog", class:'hover:text-[#FECA11] max-[800px]:text-sm' }),
+            jo("a", { t: "Contact", href: "/contact", class:'hover:text-[#FECA11] max-[800px]:text-sm' }),
           ]
         }),
 
         // user and cart
         jo('div', { 
-          class: 'flex', children: [
+          class: 'flex max-[800px]:hidden', children: [
             jo("a", {href: "/user", children:[jo('img', {src:'/img/uswer.svg'})] }),
             jo("a", {href: "/cart", children:[jo('img', {src:'/img/cart.svg'})] }),
           ]}
